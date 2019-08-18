8636a954851a951f8fd074a7bbc994143aa239b9
Nick Ninov <46753183+nickninov@users.noreply.github.com>
2019-08-18 12:03:34 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -21,12 +21,32 @@ let stateCheck = setInterval(() => {
   }
 }, 100);
 
+// Hide body 
+function bodyDisguise () {
+  document.body.style.backgroundImage = 'none';
+  document.body.style.backgroundColor = 'black';
+}
+
+function showBody () {
+  document.body.style.backgroundColor = 'none';
+
+  if(screen.width <= 700) {
+    document.body.style.backgroundImage = 'url(\'images/index/aIMG_2182_mobile.jpg\')';
+  }
+
+  if (window.matchMedia("(orientation: landscape)").matches) {
+    document.body.style.backgroundImage = 'url(\'images/index/aIMG_2182_landscape.jpg\')';
+ }
+}
+
 // About
 var aboutButton = document.getElementById('about');
 
 var aboutModal = document.getElementById('aboutWrapper');
 
 function openAboutWindow() {
+  // bodyDisguise();
+
   aboutModal.style.visibility = 'visible';
   aboutModal.classList.add("enterAnimation");
   document.getElementById('aboutMeTextWrapper').style.display = 'block';
@@ -37,17 +57,58 @@ aboutButton.addEventListener('click', openAboutWindow);
 
 var aboutCloseButton = document.getElementById('closeAbout');
 
+// Music
+var musicButton = document.getElementById('music');
+
+var musicModal = document.getElementById('musicWrapper');
+
+function openMusicWindow() {
+  musicModal.style.visibility = 'visible';
+  musicModal.classList.add("enterMusicAnimation");
+  document.getElementById('blackBackgroundWrapper').style.display = 'block';
+}
+
+musicButton.addEventListener("click", openMusicWindow);
+
+var musicCloseButton = document.getElementById('closeMusic');
+
+// Video
+var videoButton = document.getElementById('video');
+
+var videoModal = document.getElementById('videoWrapper');
+
+function openVideoWindow() {
+  videoModal.style.visibility = 'visible';
+  videoModal.classList.add("enterVideoAnimation");
+  document.getElementById('blackVideoWrapper').style.display = 'block';
+
+}
+
+videoButton.addEventListener("click", openVideoWindow);
+
+var videoCloseButton = document.getElementById('closeVideo');
 
 // Close function
 function close() {
-  // About 
+  // showBody();
+
+  // About  - hide modal and remove animation
   aboutModal.style.visibility = 'hidden';
   aboutModal.classList.remove("enterAnimation");
-  document.getElementById('aboutMeTextWrapper').style.display = 'none';
-  document.getElementById('closeAbout').style.display = 'none';
+
+  // Music - hide modal and remove animation
+  musicModal.style.visibility = 'hidden';
+  musicModal.classList.remove("enterMusicAnimation");
+  document.getElementById('blackBackgroundWrapper').style.display = 'none';
+
+  // Video - hide modal and remove animation 
+  videoModal.style.visibility = 'hidden';
+  videoModal.classList.remove("enterVideoAnimation");
+  document.getElementById('blackVideoWrapper').style.display = 'none';
 }
 
 // Click to close
 aboutCloseButton.addEventListener('click', close);
-
+musicCloseButton.addEventListener('click', close);
+videoCloseButton.addEventListener('click', close);
 
